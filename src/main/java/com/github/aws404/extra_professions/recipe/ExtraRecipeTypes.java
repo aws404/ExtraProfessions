0ecfb162afdb906d81048d6c8a2caa7f479ce9b6
aws404 <aws40404@gmail.com>
2022-05-27 22:58:47 +1000

    Update ExtraRecipeTypes.java

--- src/main/java/com/github/aws404/extra_professions/recipe/ExtraRecipeTypes.java
+++ src/main/java/com/github/aws404/extra_professions/recipe/ExtraRecipeTypes.java
@@ -13,6 +13,7 @@ public class ExtraRecipeTypes {
     public static final RecipeType<SawmillRecipe> SAWMILL_RECIPE_TYPE = registerRecipeType(id("sawmill"));
     public static final RecipeType<AnnealingRecipe> ANNEALING_RECIPE_TYPE = registerRecipeType(id("annealing"));
 
+    public static final RecipeSerializer<SawmillRecipe> SAWMILL_RECIPE_SERIALISER = Registry.register(Registry.RECIPE_SERIALIZER, id("sawmill"), new SawmillRecipe.Serialiser());
     public static final RecipeSerializer<AnnealingRecipe> ANNEALING_RECIPE_SERIALISER = Registry.register(Registry.RECIPE_SERIALIZER, id("annealing"), new CookingRecipeSerializer<>(AnnealingRecipe::new, 100));
 
     public static void init() {
